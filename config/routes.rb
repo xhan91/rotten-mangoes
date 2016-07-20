d7fbc55d3e2785615dfa8e286dc5fddff57ea86a
Josh Han <xhan91ca@gmail.com>
2016-07-20 18:27:57 +0000

    define custom prefix for URI pattern

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
+  
   resources :users, only: [:new, :create]
   resources :session, only: [:new, :create, :destroy]
+  post '/session/fake/quit', to: 'session#fake_quit'
+  post '/session/fake/:id', to: 'session#fake', as: 'session_fake'
 
   namespace :admin do
     resources :users
   end
 
-  post '/session/fake/quit', to: 'session#fake_quit'
-  post '/session/fake/:id', to: 'session#fake'
 
   root to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
